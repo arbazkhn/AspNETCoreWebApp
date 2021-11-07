d8bcc9f9a59334922777e0805539441f049d9f6b
Arbaz Khan <arbaz@Arbazs-MacBook-Pro.local>
2021-11-07 02:27:44 -0700

    change

--- Startup.cs
+++ Startup.cs
@@ -49,7 +49,7 @@ namespace WebApp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+        
             app.UseStaticFiles();
 
             app.UseRouting();
