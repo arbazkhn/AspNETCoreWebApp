a87d8ae6295d26d0318e802bf6479e1ff5820309
Arbaz Khan <arbaz@Arbazs-MacBook-Pro.local>
2021-11-07 00:52:50 -0700

    app ready to be deployed to aws

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.ViewModels;
+
+namespace WebApp.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private SignInManager<IdentityUser> signInManager;
+
+        [BindProperty]
+        public Login Model{ get; set; }
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync(string returnUrl=null)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityResult = await signInManager.PasswordSignInAsync(Model.Email,Model.Password,Model.RememberMe,false);
+                if (identityResult.Succeeded)
+                {
+                    if (returnUrl == null || returnUrl == "/")
+                    {
+                        return RedirectToPage("MainApp");
+                    }
+                    else
+                    {
+                        return RedirectToPage(returnUrl);
+                    }
+                }
+
+                
+                ModelState.AddModelError("", "Username or Password is incorrect.");
+            }
+            return Page();
+            
+        }
+    }
+}
